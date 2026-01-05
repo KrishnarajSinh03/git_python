78302b0f951f035a2c8de5275df87f759a3d5325
KrishnarajSinh03 <jadejakrishnarajsinhj@gmail.com>
2026-01-05 10:30:56 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,28 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x="hello world"
+>>> print(x)
+hello world
+>>> x="true"
+>>> print(x)
+true
+>>> x="20.5"
+>>> print(x)
+20.5
+>>> x="11"
+>>> print(x)
+11
+>>> y=["apple","banna","mango"]
+>>> print(y)
+['apple', 'banna', 'mango']
+>>> z=["guava","cherry"]
+>>> print(z)
+['guava', 'cherry']
+>>> a=
+SyntaxError: invalid syntax
+>>> c={"name","john","age":36}
+SyntaxError: invalid syntax
+>>> c={"name": "john","age":"36"}
+>>> print(c)
+{'name': 'john', 'age': '36'}
+>>> 
